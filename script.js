060e52db2b4eea0345e329a01b38f88907cef83f
nok-chan <HrithikSarkar45@protonmail.com>
2022-06-29 10:56:59 +0530

    Added a async function to get the weather data from the weather api, used resp.json() to get the JSON data. Added the search functionality by adding submit event listener to the form. It will listen for city name and take that as an input. Included another function to display the temparature data on the webpage (used createElement) and added classList .

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 const apikey = "e64f89883381962ec06b9adf929bb3d8";
 
-const APIURL = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}&units=metric`; 
+const main = document.querySelector("main");
+const form = document.querySelector("form");
+const search = document.getElementById("search");
+
+const APIURL = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
 
 
-async function getWeatherByLocation(location) {
+async function getWeatherByLocation(city) {
 
-    const resp = await fetch(APIURL(location), {origin: "cors"});
+    const resp = await fetch(APIURL(city), { origin: "cors" });
 
     const respData = await resp.json();
 
     console.log(respData, CtoF(respData.main.temp));
 
+    addWeatherToPage(respData);
+
 }
 
-getWeatherByLocation("Amsterdam");
+function addWeatherToPage(data) {
+    const temp = data.main.temp;
 
-function CtoF(C){
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `
+    <small>There are</small>
+    <h2>${temp} °C</h2>
+    <p>in ${search.value}</p>`;
+
+    // clean up
+
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function CtoF(C) {
     return Number(((C * 1.8) + 32).toFixed(2));
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if(city) {
+        getWeatherByLocation(city);
+    }
+});
